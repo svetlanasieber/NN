2c2097c72e3f738667bcdc55eb6625068002746c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-06 01:28:33 +0100

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,25 +1,25 @@
 import tensorflow as tf
-from tensorflow import keras
+import tensorflow
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
-
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),  
+    keras.layers.Flatten(input_shape=(28, 28)), 
     keras.layers.Dense(128, activation='relu'), 
-    keras.layers.Dense(10, activation='softmax') 
+    keras.layers.Dense(10, activation='softmax')
 ])
 
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
+
 
 history = model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
 
